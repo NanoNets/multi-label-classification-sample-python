2bb44f877f0868b7c615f2fa42d18a8dd59c8280
aatishpatel <atish@nanonets.com>
2018-11-16 19:24:46 +0530

    batch upload

--- code/upload_training.py
+++ code/upload_training.py
@@ -22,21 +22,31 @@ def create_image_dictionary():
 
 def upload_images(model_id):
     url = BASE_URL + 'Model/%s/UploadFiles/'%(model_id)
+    print ("Uploading Images......")
     image_label_dictionary = create_image_dictionary()
     all_images = image_label_dictionary.keys()
     random.shuffle(all_images)
     n = len(all_images)
-    for i, image in enumerate(all_images):
-        if i%100==0:
-            print ("%d of %d images has been uploaded"%(i, n))
-        labels = image_label_dictionary[image]
-        _, image_name = os.path.split(image)
-        files = open(image, 'rb')
-        data = {'files' : files,
-                'data' :('', '[{"filename":"%s", "categories": %s}]'%(image_name, json.dumps(labels)))}
-        response = requests.post(url, auth= requests.auth.HTTPBasicAuth(AUTH_KEY, ''), files=data)
-    print ("%d of %d images has been uploaded"%(i, n))
-    return json.loads(response.text)
+    image_uploaded = 0
+    while len(all_images) > 0:
+        batch_images, all_images = all_images[:50], all_images[50:]
+        multiple_files = []
+        multiple_data = []
+        for image in batch_images:
+            labels = image_label_dictionary[image]
+            image_dir, image_name = os.path.split(image)
+            image_data = {'filename': image_name, "categories": labels}
+            multiple_data.append(image_data)
+            multiple_files.append(('files', (image_name, open(image, 'rb'), 'image/jpeg')))
+        multiple_files.append(('data', ('', json.dumps(multiple_data))))
+        response = requests.post(url, auth= requests.auth.HTTPBasicAuth(AUTH_KEY, ''), files=multiple_files)
+        image_uploaded += len(batch_images)
+        if len(all_images) > 0:
+            print ("%d of %d images has been uploaded, uploading next batch...."%(image_uploaded, n))
+        else:
+            print ("%d of %d images has been uploaded, Done uploading"%(image_uploaded, n))
+    print get_model(model_id)
+    return 
 
 if __name__=="__main__":
     upload_images(MODEL_ID)
